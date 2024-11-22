8b3475b45d2d1f6ae59f6a789d36c920116c87ec
Michael <motlokwa.thulare@gmail.com>
2024-11-22 14:52:52 +0200

    ggr

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -11,7 +11,7 @@ router.put("/update/:id",protect,updateUser);
 router.put("/deactivate/:id",protect,DeactivateUser);
 router.put("/activate/:id",protect,activateUser);
 router.get("/getUserbyemail",protect,getUserByEmail);
-router.put("/suspend/:id",suspendAccount);
+router.put("/suspend/:id",protect,suspendAccount);
 // router.delete("/delete/:id",deleteUser);
 
 export default router;
