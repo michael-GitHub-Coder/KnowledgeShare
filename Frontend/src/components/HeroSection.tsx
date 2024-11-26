39e2ccfb293267e2e466e3194465ea4b9437bbcc
cass-us <cassiusmaropene@gmail.com>
2024-11-26 14:16:51 +0200

    see

--- Frontend/src/components/HeroSection.tsx
+++ Frontend/src/components/HeroSection.tsx
@@ -1,8 +1,14 @@
-
+import { useState } from 'react';
+import { IoIosSearch } from "react-icons/io";
+import { MdArrowDropDown } from "react-icons/md";
 import logo from "./assets/logo.png";
-import backgroundImage from "./assets/background-2.png"; 
+import backgroundImage from "./assets/background-2.png";
 
 const HeroSection = () => {
+    const [showCategoryDropdown, setShowCategoryDropdown] = useState(false);
+
+    const toggleCategoryDropdown = () => setShowCategoryDropdown(!showCategoryDropdown);
+
     return (
         <section
             className="bg-cover bg-center h-[200px]" 
@@ -10,21 +16,21 @@ const HeroSection = () => {
                 backgroundImage: `url(${backgroundImage})`,
             }}
         >
-            {/* Logo */}
-            <img src={logo} className="w-64 h-32 absolute left-0 top-0 ml-20" alt="Logo" />
+        
+            <img src={logo} className="w-64 h-48 mt-0 absolute  ml-20" alt="Logo" />
 
-            {/* Navigation Bar */}
+        
             <nav className="bg-gray-100 bg-opacity-70">
                 <div className="flex items-center text-center ml-[800px] justify-between px-6 py-2">
                     <ul className="flex space-x-6">
                         <li>
-                            <a href="/" className="text-blue-500 hover:underline">Blogs</a>
+                            <a href="/" className="text-black hover:underline">Blogs</a>
                         </li>
                         <li>
-                            <a href="/" className="text-blue-500 hover:underline">Trending</a>
+                            <a href="/" className="text-black hover:underline">Trending</a>
                         </li>
                         <li>
-                            <a href="/" className="text-blue-500 hover:underline">Latest</a>
+                            <a href="/" className="text-black hover:underline">Latest</a>
                         </li>
                     </ul>
                     <div className="flex space-x-6 mt-2 mr-8">
@@ -33,11 +39,35 @@ const HeroSection = () => {
                     </div>
                 </div>
             </nav>
-            <div className="text-3xl font-bold text-center p-4">
-               <h1>
-                  vc
-               </h1>
+
+            <div className="flex space-x-4 text-center justify-center mt-8 ">
+               
+                <input
+                    type="text"
+                    placeholder="Search author..."
+                    className="p-1 w-[400px] rounded-xl text-black"
+                />
+                 <IoIosSearch className="w-8 mt-2" size={20} />
+    
+                <div className="relative">
+                    <button 
+                        onClick={toggleCategoryDropdown} 
+                        className="bg-gray-200 rounded-xl px-4 py-2 flex items-center"
+                    >
+                        Categories
+                        <MdArrowDropDown className="ml-2" />
+                    </button>
+                    {showCategoryDropdown && (
+                        <div className="absolute top-full left-0 mt-2 w-48 bg-white shadow-lg rounded-xl p-2">
+                            <a href="/" className="block px-4 py-2 text-blue-500 hover:bg-gray-100">Technology</a>
+                            <a href="/" className="block px-4 py-2 text-blue-500 hover:bg-gray-100">Health</a>
+                            <a href="/" className="block px-4 py-2 text-blue-500 hover:bg-gray-100">Lifestyle</a>
+                            <a href="/" className="block px-4 py-2 text-blue-500 hover:bg-gray-100">Business</a>
+                        </div>
+                    )}
+                </div>
             </div>
+
         </section>
     );
 };
---
+++ Frontend/src/components/card.tsx
@@ -0,0 +1,11 @@
+
+
+const card = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default card
