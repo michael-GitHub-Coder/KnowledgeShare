08d43c86c6e4364414e43951499867adada5a50e
Michael <motlokwa.thulare@gmail.com>
2024-11-26 11:40:10 +0200

    dfdg

--- Frontend/src/Components/Card.tsx
+++ Frontend/src/Components/Card.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { FaGreaterThan } from "react-icons/fa6";
 import { FaComments } from "react-icons/fa";
 import { MdOutlinePerson2 } from "react-icons/md";
 
 const Card = () => {
   return (
-    <div className="flex justify-center mt-[100px]">
+    <div className="flex justify-center mt-[100px] ">
       <div className="relative shadow-lg">
         <div className="relative bg-gray-100 w-[350px] h-[250px] mr-[10%]">
           <img src="https://www.topgear.com/sites/default/files/2022/07/9_1.jpg" className="w-full h-full object-cover" />
@@ -13,11 +14,14 @@ const Card = () => {
             <p className="bg-white text-black font-bold px-2 py-2 text-center">Nov</p>
           </div>
         </div>
-        <div className="absolute bg-white h-[268px] w-[320px] top-[80%] -right-[10%] left-0 z-10 shadow-sm p-5 text-gray-500 ">
+        <div className="absolute bg-white h-[268px] w-[320px] top-[80%] -right-[10%] left-0 z-10 shadow-sm p-5 text-gray-500 px-5">
             <div className="flex gap-4 border-b border-b-gray-500 pb-1 ">
                 <p className="flex gap-2"><MdOutlinePerson2 className="mt-1"/> Michael T.</p>
                 <p className="flex gap-2"><FaComments className="mt-1"/>Comments. (5)</p>
             </div>
+            <h1 className="py-4 font-bold text-black">Navigating Borders ULtimate Guide to Visa Success</h1>
+            <p className="pb-4">Navigating Borders ULtimate Guide to Visa Success, ULtimate Navigating Borders ULtimate Guide... </p>
+            <p className="pb-4 flex gap-2 font-bold text-black">Read More <FaGreaterThan size={10} className="mt-2"/></p>
         </div>
       </div>
     </div>
