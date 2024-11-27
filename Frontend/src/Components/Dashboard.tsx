e7b7c0d18d75f5f5c2690d7bfbc9ee745f3907bd
Michael <motlokwa.thulare@gmail.com>
2024-11-27 17:59:34 +0200

    fewf

--- Frontend/src/Components/Dashboard.tsx
+++ Frontend/src/Components/Dashboard.tsx
@@ -86,7 +86,7 @@ const Dashboard = () => {
         </nav>
       </div>
 
-      <div className="flex-1 p-6">
+      <div className="flex-1 p-6 bg-gray-100">
         <div className="flex justify-between px-5">
           <h1 className="text-2xl mb-10">Welcome to Dashboard</h1>
           <div className="flex gap-2 relative">
