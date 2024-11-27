9d32ab1b97e6d04a76a4a590f34521e108ab539c
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-11-27 14:24:29 +0200

    ...

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,44 +1,51 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Card from './Components/Card'
-// import Dashboard from './Components/Dashboard'
+import Dashboard from './Components/Dashboard'
 import Login from './Components/Login'
 import Register from './Components/Register'
+import ManageUser from './Components/ManageUser'
+import ManageGuides from './Components/ManageGuides'
+import PlatformStats from './Components/PlatformStats'
+import Analytics from './Components/Analytics'
+import AddEditGuide from './Components/AddEditGuide'
+import Footer from './Components/Footer'
+
 
 const App = () => {
-  const data = [
-    {
-      "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
-      "title": "The Ultimate Guide to React",
-      "content": "Learn the fundamentals of React and how to build dynamic web applications using this popular JavaScript library."
-    },
-    {
-      "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
-      "title": "Mastering JavaScript ES6",
-      "content": "Dive deep into the new features of JavaScript ES6 and how they improve the development process."
-    },
-    {
-      "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
-      "title": "Understanding TypeScript",
-      "content": "A comprehensive guide to TypeScript, offering better code quality and development experience with its type system."
-    }
-  ];
+  // const data = [
+  //   {
+  //     "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
+  //     "title": "The Ultimate Guide to React",
+  //     "content": "Learn the fundamentals of React and how to build dynamic web applications using this popular JavaScript library."
+  //   },
+  //   {
+  //     "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
+  //     "title": "Mastering JavaScript ES6",
+  //     "content": "Dive deep into the new features of JavaScript ES6 and how they improve the development process."
+  //   },
+  //   {
+  //     "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
+  //     "title": "Understanding TypeScript",
+  //     "content": "A comprehensive guide to TypeScript, offering better code quality and development experience with its type system."
+  //   }
+  // ];
   
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />}>
-          <Route index element={<h3>Dashboard Home</h3>} /> 
-          <Route path="ManageUser" element={<ManageUser />} />
-          <Route path="ManageGuides" element={<ManageGuides />} />
-          <Route path="PlatformStats" element={<PlatformStats />} />
-        </Route>
-        <Route path="/register" element={<Register />} />
-      </>
-    )
-  );
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <>
+  //       <Route path="/" element={<Login />} />
+  //       <Route path="/Dashboard" element={<Dashboard />}>
+  //         <Route index element={<h3>Dashboard Home</h3>} /> 
+  //         <Route path="ManageUser" element={<ManageUser />} />
+  //         <Route path="ManageGuides" element={<ManageGuides />} />
+  //         <Route path="PlatformStats" element={<PlatformStats />} />
+  //       </Route>
+  //       <Route path="/register" element={<Register />} />
+  //     </>
+  //   )
+  // );
 
 
   return (
@@ -51,7 +58,10 @@ const App = () => {
           ))  
         }
       </div> */}
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <Analytics/>
+      <AddEditGuide/>
+      <Footer/>
     </>
    
   )
