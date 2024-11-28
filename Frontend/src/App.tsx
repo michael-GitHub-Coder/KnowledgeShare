126ac70145686bb5d76a7ef5652c8a49fe960d2d
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-11-28 10:43:47 +0200

    ..

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -15,23 +15,23 @@ import Profile from './Components/Profile'
 
 
 const App = () => {
-  // const data = [
-  //   {
-  //     "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
-  //     "title": "The Ultimate Guide to React",
-  //     "content": "Learn the fundamentals of React and how to build dynamic web applications using this popular JavaScript library."
-  //   },
-  //   {
-  //     "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
-  //     "title": "Mastering JavaScript ES6",
-  //     "content": "Dive deep into the new features of JavaScript ES6 and how they improve the development process."
-  //   },
-  //   {
-  //     "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
-  //     "title": "Understanding TypeScript",
-  //     "content": "A comprehensive guide to TypeScript, offering better code quality and development experience with its type system."
-  //   }
-  // ];
+  const data = [
+    {
+      "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
+      "title": "The Ultimate Guide to React",
+      "content": "Learn the fundamentals of React and how to build dynamic web applications using this popular JavaScript library."
+    },
+    {
+      "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
+      "title": "Mastering JavaScript ES6",
+      "content": "Dive deep into the new features of JavaScript ES6 and how they improve the development process."
+    },
+    {
+      "image": "https://4kwallpapers.com/images/wallpapers/bugatti-w16-mistral-sports-cars-ultimate-roadster-2024-4500x3000-9103.jpeg",
+      "title": "Understanding TypeScript",
+      "content": "A comprehensive guide to TypeScript, offering better code quality and development experience with its type system."
+    }
+  ];
   
 
   const router = createBrowserRouter(
@@ -49,22 +49,9 @@ const App = () => {
     )
   );
 
+}
 
-  return (
-    <>
-      {/* <Login /> */}
-      {/* <Register /> */}
-      {/* <div className="flex justify-center gap-8">
-        {  data.map(data=>( 
-            <Card image={data.image} title={data.title} content={data.content}/>
-          ))  
-        }
-      </div> */}
-      {/* <Dashboard /> */}
-      {/* <Analytics/>
-      <AddEditGuide/>
-      <Footer/>
-    </> */}
+     
    
 
 
